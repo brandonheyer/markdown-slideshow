9a67417147977748fcfca148d8d7ee7c169faea6
Brandon Heyer <brandon@crowdskout.com>
2017-11-06 11:12:09 -0500

    initial commit, demo, instructions

--- README.md
+++ README.md
@@ -1,1 +1,113 @@
 # markdown-slideshow
+
+Convert a markdown document into an HTML and JS based slideshow.
+
+# Markdown Setup
+The markdown file should follows some simple rules:
+* **`#` level headers are ignored by default**, they should be used for dictating structure in your presentation
+* **`##` level headers indicate a new slide**, they are also the primary header for the new slide. Headers can end in `ID:SOME_STRING` to denote a unique identifier for that header. This can then be used for the `sectionPreprocess` configuration (see below).
+* **Paragraphs are ignored by default**, they should be used for presenter notes
+* All other elements reflect their appropriate HTML elements and can be styled as such.
+
+# File Setup
+## HTML
+```
+<html>
+  <head>
+    <title>Markdown Slideshow Demo</title>
+
+    <link href="../node_modules/prismjs/themes/prism.css" rel="stylesheet"> </link>
+
+    <script src="../node_modules/jquery/dist/jquery.min.js"> </script>
+    <script src="../node_modules/showdown/dist/showdown.min.js"> </script>
+    <script src="../node_modules/prismjs/prism.js"> </script>
+    <script src="../node_modules/markdown-presentation/src/markdown-presentation.js"> </script>
+  </head>
+  <body>
+    <div class="presentation-parent"> </div>
+
+    <script>
+      var pres = new MarkdownPresentation({
+        presentationElement: '.presentation-parent',
+        data: mySlideData
+      });
+
+      pres.start();
+    </script>
+  </body>
+</html>
+```
+
+# Configuration
+## Configuration Object
+```JavaScript
+{
+  presentationElement: '.foo',
+  data: `
+# Intro
+## Slide 1
+## Slide 2
+# Closing
+## Slide 3 ID: slide3
+`,
+  sectionPreprocess: {
+    'slide3': function() {
+      // Do some stuff when the slide is opened
+
+      return function() {
+        // Do some stuff when the slide is closed
+      }
+    }
+  }
+
+}
+```
+
+# Grunt Setup
+
+If you wish to use a separate `.md` file for your slide document, you will either
+need to manually copy over the markdown into JavaScript, run the code on a local
+or remote webserver, or use something like Grunt to manage the copying for you.
+
+To help with the setup, I've included an example grunt config below, this uses
+`fs` and `grunt-replace`. This set up assumes you name the markdown file and
+the resulting
+
+## Gruntfile.js
+```JavaScript
+var sourceFile = './presentation';
+var destinationDir = './dist';
+var fs = require('fs');
+var markdown = fs.readFileSync(sourceFile + '.md', { encoding: 'utf8' });
+
+markdown = markdown.replace(/```/g, '\\`\\`\\`');
+
+module.exports = function (grunt) {
+  var config = {
+    replace: {
+      dev: {
+        options: {
+          patterns: [
+            {
+              match: 'MARKDOWN',
+              replacement: markdown
+            }
+          ],
+        },
+        files: [{
+          src: [sourceFile + '.js'],
+          dest: destinationDir
+        }]
+      }
+    }
+  };
+}
+```
+
+## presentation.js
+```
+var data =
+`
+@@MARKDOWN
+`;
+```
---
+++ src/markdown-presentation.js
@@ -0,0 +1,134 @@
+(function(window, $, showdown, Prism) {
+  var MDP = window.MarkdownPresentation = function(config) {
+    if (!config.presentationElement) {
+      throw 'Must configure a presentationElement selector';
+    }
+
+    this.presentationElement = $(config.presentationElement);
+    this.data = config.data;
+    this.sectionPreprocessLookup = {} || config.sectionPreprocess;
+    this.sections = [];
+    this.current = 0;
+  };
+
+  MDP.prototype.tagLookup = {
+    'H1': function() {
+      return false;
+    },
+    'P': function() {
+      return false;
+    },
+    'H2': function(element, sections) {
+      var id = element.innerText.split('ID:');
+      var section;
+
+      element.innerText = id[0];
+
+      section = $('<section data-id="' + id[1] + '">')
+      sections.push(section);
+
+      return section;
+    }
+  };
+
+  MDP.prototype.updateCurrent = function() {
+    var lookup = this.sectionPreprocessLookup[
+      this.sections[this.current].attr('data-id')
+    ];
+
+    if (lookup) {
+      this.cleanup = lookup();
+    }
+
+    if (Prism) {
+      this.sections[this.current]
+        .find('code')
+        .html(
+          Prism.highlight(
+            this.sections[this.current].find('code')
+              .text(),
+            Prism.languages.javascript
+          )
+        );
+    }
+  }
+
+  MDP.prototype.onKeyup = function(event) {
+    var oldCurrent = this.current;
+
+    if (event.which === 37) {
+      if (this.current > 0) {
+        this.current--;
+      }
+    } else if (event.which === 32 || event.which === 13) {
+      if (this.current < this.sections.length - 1) {
+        this.current++;
+      }
+    }
+
+    if (this.current !== this.oldCurrent) {
+      if (this.cleanup) {
+        this.cleanup();
+      }
+
+      this.presentationElement.children()
+        .first()
+          .detach()
+          .hide();
+
+      this.sections[this.current].appendTo(
+        this.presentationElement.empty()
+      ).fadeIn();
+
+      this.updateCurrent();
+
+      window.location.hash = this.current;
+    }
+  }
+
+  MDP.prototype.start = function() {
+    var that = this;
+    var currentSection;
+
+    if (this.presentationElement.length) {
+      $(
+        (new showdown.Converter({
+          disableForced4SpacesIndentedSublists: true
+        }))
+        .makeHtml(this.data)
+      ).each(function(i, element) {
+        var result;
+
+        if (element.tagName === undefined) {
+          return;
+        }
+
+        if (that.tagLookup[element.tagName]) {
+          result = currentSection = that.tagLookup[element.tagName](element, that.sections);
+        }
+
+        if (result === false) {
+          return;
+        }
+
+        currentSection.append(element);
+      });
+
+      if (window.location.hash) {
+        this.current = parseInt(window.location.hash.replace('#', ''), 10);
+      }
+
+      this.presentationElement
+        .empty()
+        .append(this.sections[this.current]);
+
+      this.sections[this.current].fadeIn();
+
+      this.updateCurrent();
+
+      $('body').keyup(function(event) {
+        that.onKeyup(event);
+      });
+    }
+  };
+})(window, $, showdown, Prism);
